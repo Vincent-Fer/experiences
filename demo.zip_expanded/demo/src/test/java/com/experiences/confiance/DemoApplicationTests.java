0b595a7753058e3a29a5b0f4cb711d54caeb7fed
Vincent Fer <vincent.fer@imt-atlantique.fr>
2026-02-10 13:52:01 +0100

    Premier commit

---
+++ demo.zip_expanded/demo/src/test/java/com/experiences/confiance/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experiences.confiance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
