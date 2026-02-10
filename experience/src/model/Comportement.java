7c51069116ba9cae0e38eb617cdb866a9bb93424
Vincent Fer <vincent.fer@imt-atlantique.fr>
2026-02-10 14:23:36 +0100

    commit

--- experience/src/model/Comportement.java
+++ experience/src/model/Comportement.java
@@ -1,5 +1,29 @@
 package model;
 
 public class Comportement {
+	
+	private String nom;
+	private Integer value;
+	
+	public Comportement(String nom, Integer value) {
+		this.setNom(nom);
+		this.setValue(value);
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public Integer getValue() {
+		return value;
+	}
+
+	public void setValue(Integer value) {
+		this.value = value;
+	}
 
 }
