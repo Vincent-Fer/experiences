52f2d0494494340e1db4e4fdf5a8c49a430a13a8
Vincent Fer <vincent.fer@imt-atlantique.fr>
2026-02-11 16:51:42 +0100

    commit

---
+++ interface_web/src/main/java/com/projet/webapp/model/User.java
@@ -0,0 +1,20 @@
+package com.projet.webapp.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+	
+	private Integer id;
+	
+	private String nom;
+	
+	private String prenom;
+	
+	private String mail;
+	
+	private String login;
+	
+	private String password;
+
+}
---
+++ interface_web/src/main/java/com/projet/webapp/repository/UserProxy.java
@@ -0,0 +1,21 @@
+package com.projet.webapp.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import com.projet.webapp.CustomProperties;
+import com.projet.webapp.model.User;
+
+import lombok.Data;
+
+
+@Data
+public class UserProxy {
+	
+	
+
+}
