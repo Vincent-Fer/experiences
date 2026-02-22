09bbbcced4b26b8d456b920d8a205b0460529c44
Vincent Fer <fer.vincent@proton.me>
2026-02-22 21:43:40 +0100

    ajout test

--- api/src/test/java/com/projet/api/UserControllerTests.java
+++ api/src/test/java/com/projet/api/UserControllerTests.java
@@ -2,28 +2,30 @@ package com.projet.api;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.projet.api.controller.UserController;
-import com.projet.api.service.UserService;
-
-@WebMvcTest(controllers = UserController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class UserControllerTests {
 	
 	@Autowired
 	private MockMvc mockMvc;
 	
-	@MockitoBean
-	private UserService userService;
+	@Test
+    void contextLoads() {
+    }
 	
 	@Test
 	public void testGetUsers() throws Exception{
-		mockMvc.perform(get("/users")).andExpect(status().isOk());
+		mockMvc.perform(get("/users"))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$[0].firstName", is("Vincent")));
 	}
 }
